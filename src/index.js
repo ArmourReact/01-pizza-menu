4e03cc154c5b66c2d829d446abb393b313ed6eb1
Jake Armour <github@jakearmour.com>
2023-07-11 11:56:45 +0100

    feat: add pizza component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+    </div>
+  );
+};
   return <h1>Hello React</h1>;
+
+const Pizza = () => {
+  return (
+    <div>
+      <img alt="Pizza Spinaci" src="pizzas/spinaci.jpg" />
+      <h2>Pizza Spinaci</h2>
+      <p>ingrediants</p>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
