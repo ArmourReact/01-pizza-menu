62aa89afbd9501e2c3c41b7f0493c66570815b81
Jake Armour <github@jakearmour.com>
2023-07-11 13:25:18 +0100

    feat: conditional rendering on footer

--- src/index.js
+++ src/index.js
@@ -56,7 +56,12 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open!
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 };
